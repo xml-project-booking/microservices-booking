03ae7ffb76773315a2831fe9e5878d0522787a0e
Kumici13 <lukapikula13@gmail.com>
2023-06-19 11:00:15 +0200

    Better

--- microservices_demo-master/common/proto/term_service/term_service.pb.gw.go
+++ microservices_demo-master/common/proto/term_service/term_service.pb.gw.go
@@ -255,6 +255,74 @@ func local_request_TermService_Delete_0(ctx context.Context, marshaler runtime.M
 
 }
 
+func request_TermService_DeleteInPeriod_0(ctx context.Context, marshaler runtime.Marshaler, client TermServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteInPeriodRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DeleteInPeriod(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_TermService_DeleteInPeriod_0(ctx context.Context, marshaler runtime.Marshaler, server TermServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteInPeriodRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.DeleteInPeriod(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_TermService_UpdateInPeriod_0(ctx context.Context, marshaler runtime.Marshaler, client TermServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateInPeriodRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.UpdateInPeriod(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_TermService_UpdateInPeriod_0(ctx context.Context, marshaler runtime.Marshaler, server TermServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateInPeriodRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.UpdateInPeriod(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterTermServiceHandlerServer registers the http handlers for service TermService to "mux".
 // UnaryRPC     :call TermServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -383,6 +451,56 @@ func RegisterTermServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		}
 
 		forward_TermService_Delete_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_TermService_DeleteInPeriod_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/terms.TermService/DeleteInPeriod", runtime.WithHTTPPathPattern("/terms/term/delete-in-period"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TermService_DeleteInPeriod_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TermService_DeleteInPeriod_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_TermService_UpdateInPeriod_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/terms.TermService/UpdateInPeriod", runtime.WithHTTPPathPattern("/terms/term/update-in-period"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TermService_UpdateInPeriod_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TermService_UpdateInPeriod_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -537,6 +655,50 @@ func RegisterTermServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_TermService_DeleteInPeriod_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/terms.TermService/DeleteInPeriod", runtime.WithHTTPPathPattern("/terms/term/delete-in-period"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TermService_DeleteInPeriod_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TermService_DeleteInPeriod_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_TermService_UpdateInPeriod_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/terms.TermService/UpdateInPeriod", runtime.WithHTTPPathPattern("/terms/term/update-in-period"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TermService_UpdateInPeriod_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TermService_UpdateInPeriod_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -550,6 +712,10 @@ var (
 	pattern_TermService_Update_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"terms", "term", "id"}, ""))
 
 	pattern_TermService_Delete_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"terms", "term", "id"}, ""))
+
+	pattern_TermService_DeleteInPeriod_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"terms", "term", "delete-in-period"}, ""))
+
+	pattern_TermService_UpdateInPeriod_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"terms", "term", "update-in-period"}, ""))
 )
 
 var (
@@ -562,4 +728,8 @@ var (
 	forward_TermService_Update_0 = runtime.ForwardResponseMessage
 
 	forward_TermService_Delete_0 = runtime.ForwardResponseMessage
+
+	forward_TermService_DeleteInPeriod_0 = runtime.ForwardResponseMessage
+
+	forward_TermService_UpdateInPeriod_0 = runtime.ForwardResponseMessage
 )
--- microservices_demo-master/term_service/infrastructure/api/term_grpc_api.go
+++ microservices_demo-master/term_service/infrastructure/api/term_grpc_api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/sirupsen/logrus"
 	pb "github.com/tamararankovic/microservices_demo/common/proto/term_service"
@@ -51,6 +52,10 @@ func (handler *TermHandler) Update(ctx context.Context, request *pb.UpdateReques
 	layout := "2006-01-02T15:04:05.000Z"
 	id, _ := primitive.ObjectIDFromHex(request.Id)
 	accId, _ := primitive.ObjectIDFromHex(request.AccommodationId)
+	if !requestIsValid(request.PriceType, int32(request.Value), accId) {
+		return nil, errors.New("Cannot accomodation, value or type can't be zero")
+	}
+
 	usrId, _ := primitive.ObjectIDFromHex(request.UserId)
 
 	date, _ := time.Parse(layout, request.Date)
@@ -157,10 +162,15 @@ func (handler *TermHandler) Create(ctx context.Context, request *pb.CreateReques
 	//KONVERZIJA IZ STRINGA U ODGOVARAJUCE TIPOVE
 	layout := "2006-01-02T15:04:05.000Z"
 	accId, _ := primitive.ObjectIDFromHex(request.AccommodationId)
+	if !requestIsValid(request.PriceType, int32(request.Value), accId) {
+		return nil, errors.New("Accomodation, value or type can't be zero")
+	}
 	usrId, _ := primitive.ObjectIDFromHex(request.UserId)
 
 	strtDate, _ := time.Parse(layout, request.StartDate)
 	eDate, _ := time.Parse(layout, request.EndDate)
+	strtDate = strtDate.Truncate(24 * time.Hour)
+	eDate = eDate.Truncate(24 * time.Hour)
 
 	//PROVJERE
 
@@ -197,4 +207,174 @@ func (handler *TermHandler) Create(ctx context.Context, request *pb.CreateReques
 	}
 
 	return response, nil
+}
+
+func (handler *TermHandler) UpdateInPeriod(ctx context.Context, request *pb.UpdateInPeriodRequest) (*pb.UpdateResponse, error) {
+	// Convert request to JSON
+	jsonBytes, err := protojson.Marshal(request)
+	if err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status":    "failure",
+			"location":  "Term Handler",
+			"action":    "CRADA731",
+			"timestamp": time.Now().String(),
+		}).Error("Failed to cast JSON to Term!")
+		return nil, err
+	}
+
+	// Unmarshal JSON to request object
+	updateRequest := &pb.UpdateInPeriodRequest{}
+	err = protojson.Unmarshal(jsonBytes, updateRequest)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// Convert string IDs to primitive.ObjectIDs
+	accId, _ := primitive.ObjectIDFromHex(updateRequest.AccommodationId)
+
+	if !requestIsValid(updateRequest.PriceType, int32(updateRequest.Value), accId) {
+		return nil, errors.New("Cannot accomodation, value or type can't be zero")
+	}
+	usrId, _ := primitive.ObjectIDFromHex(updateRequest.UserId)
+
+	layout := "2006-01-02T15:04:05.000Z"
+	// Parse start and end date strings to time.Time
+	startDate, _ := time.Parse(layout, updateRequest.StartDate)
+	endDate, _ := time.Parse(layout, updateRequest.EndDate)
+	startDate = startDate.Truncate(24 * time.Hour)
+	endDate = endDate.Truncate(24 * time.Hour)
+
+	// Get all terms
+	terms, err := handler.service.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if any existing term in the specified period has a non-nil or non-zero user ID
+	existingTerm := findTermInPeriodWithUserId(terms, accId, startDate, endDate)
+	if existingTerm != nil {
+		return nil, errors.New("Cannot update/create terms. An existing term in the period already has a non-nil or non-zero user ID")
+	}
+
+	// Iterate through dates in the period
+	for date := startDate; date.Before(endDate); date = date.AddDate(0, 0, 1) {
+		// Check if a term exists for the date and accommodation ID
+		existingTerm := findTermByDateAndAccommodation(terms, date, accId)
+		if existingTerm != nil {
+			// Update the existing term with new information
+			existingTerm.UserID = usrId
+			existingTerm.PriceType = updateRequest.PriceType
+			existingTerm.Value = int32(updateRequest.Value)
+			err = handler.service.Update(existingTerm)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Create a new term for the date and accommodation ID
+			newTerm := domain.NewTerm(accId, usrId, updateRequest.PriceType, int32(updateRequest.Value), date)
+			err = handler.service.Create(newTerm)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// Prepare the response
+	res := pb.UpdateResponse{}
+
+	return &res, nil
+}
+
+// Helper function to check if request is valid
+func findTermByDateAndAccommodation(terms []*domain.Term, date time.Time, accId primitive.ObjectID) *domain.Term {
+	for _, term := range terms {
+		if term.Date.Equal(date) && term.AccommodationID == accId {
+			return term
+		}
+	}
+	return nil
+}
+
+// Helper function to checkIfRequestIsValid
+func requestIsValid(priceType string, value int32, accId primitive.ObjectID) bool {
+	if accId.IsZero() {
+		return false
+	}
+
+	if priceType == "" {
+		return false
+	}
+
+	if value <= 0 {
+		return false
+	}
+
+	return true
+}
+
+// Helper function to check if any existing term in the specified period has a non-nil or non-zero user ID
+func findTermInPeriodWithUserId(terms []*domain.Term, accId primitive.ObjectID, startDate, endDate time.Time) *domain.Term {
+	for _, term := range terms {
+		if term.AccommodationID == accId && term.Date.After(startDate) && term.Date.Before(endDate) {
+			if !term.UserID.IsZero() {
+				return term
+			}
+		}
+	}
+	return nil
+}
+
+func (handler *TermHandler) DeleteInPeriod(ctx context.Context, request *pb.DeleteInPeriodRequest) (*pb.DeleteResponse, error) {
+	// Convert request to JSON
+	jsonBytes, err := protojson.Marshal(request)
+	if err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status":    "failure",
+			"location":  "Term Handler",
+			"action":    "CRADA731",
+			"timestamp": time.Now().String(),
+		}).Error("Failed to cast JSON to Term!")
+		return nil, err
+	}
+
+	// Unmarshal JSON to request object
+	deleteRequest := &pb.DeleteInPeriodRequest{}
+	err = protojson.Unmarshal(jsonBytes, deleteRequest)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// Convert string IDs to primitive.ObjectIDs
+	accId, _ := primitive.ObjectIDFromHex(deleteRequest.AccommodationId)
+	layout := "2006-01-02T15:04:05.000Z"
+
+	// Parse start and end date strings to time.Time
+	startDate, _ := time.Parse(layout, deleteRequest.StartDate)
+	endDate, _ := time.Parse(layout, deleteRequest.EndDate)
+	startDate = startDate.Truncate(24 * time.Hour)
+	endDate = endDate.Truncate(24 * time.Hour)
+
+	// Get all terms
+	terms, err := handler.service.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Iterate through terms, delete matching ones
+	for _, term := range terms {
+
+		if term.AccommodationID == accId && term.Date.After(startDate) && term.Date.Before(endDate) {
+			err = handler.service.Delete(term)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// Prepare the response
+	res := pb.DeleteResponse{}
+
+	return &res, nil
 }
